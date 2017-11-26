cf3ec959b7251b492fb01b1d265b442ff7bd1fff
Leonel Castañeda <leodcastaneda@gmail.com>
2017-11-26 13:37:00 -0600

    indicators update

--- client/resources/js/indicators.js
+++ client/resources/js/indicators.js
@@ -1,13 +1,13 @@
-
 function getTopDangerous(){
+  $("#chargingDialog").modal('show');
+
   hideZonesLayer();
   hideRouteLayer();
 
   var maxResultSize = 10;
-
   var acceptedTypes = ["Incidente vial"];
 
-  var incidentes = {
+  incidentes = {
     type: "FeatureCollection",
     features: getPointsInView(acceptedTypes)
   }
@@ -15,7 +15,6 @@ function getTopDangerous(){
   var pointBuffer, cluster, clusters = [];
   for(var feature of incidentes.features){
     pointBuffer = turf.buffer(feature, 0.020, {units: "kilometers"});
-
     cluster = turf.within(incidentes, turf.featureCollection([pointBuffer]));
 
     clusters.push({
@@ -35,13 +34,10 @@ function getTopDangerous(){
     cluster = clusters[i];
 
     if(counted.indexOf(cluster.feature.properties._index) < 0){
-
       counted.push(cluster.feature.properties._index);
 
       cluster.feature.properties._index = result.features.length+1;
       cluster.feature.properties.counter = cluster.cluster.features.length;
-
-
       result.features.push(cluster.feature);
 
       for(var feature of cluster.cluster.features){
@@ -50,24 +46,74 @@ function getTopDangerous(){
     }
   }
 
-  var html = "indice, numero_incidentes, latitud, longitud\n";
-  for(var i = 0; i < result.features.length; i++){
-    feature = result.features[i];
-    html += (i+1) + ", " + feature.properties.counter + ", " + feature.geometry.coordinates[1] + ", " + feature.geometry.coordinates[0] + "\n";
-  }
+  // get info
+  reverseGeocoding(result, 0, function(result){
+    var html = "indice, numero_incidentes, calle_1, calle_2, codigo_postal, limite_velocidad, limite_velocidad_unidades, latitud, longitud\n";
+    for(var i = 0; i < result.features.length; i++){
+      feature = result.features[i];
 
-  $("#resultTopDangerous").html(html);
-  // update map
-  setPointFilters(acceptedTypes);
+      html += (i+1) + ", " +
+        feature.properties.counter + ", " +
+        feature.properties.calle_1 + ", " +
+        (feature.properties.calle_2 ? feature.properties.calle_2:"-") + ", " +
+        feature.properties.codigo_postal + ", " +
+        (feature.properties.limite_velocidad ? feature.properties.limite_velocidad:"") + ", " +
+        (feature.properties.limite_velocidad_unidades ? feature.properties.limite_velocidad_unidades:"") + ", " +
+        feature.geometry.coordinates[1] + ", " +
+        feature.geometry.coordinates[0] + "\n";
+    }
 
-  indicatorsLayer.clearLayers();
-  indicatorsLayer.addData(result);
+    $("#resultTopDangerous").html(html);
+
+    // update map
+    setPointFilters(acceptedTypes);
+
+    indicatorsLayer.clearLayers();
+    indicatorsLayer.addData(result);
+
+    $("#downloadTopDangerous").removeClass("hidden");
+    $("#chargingDialog").modal('hide');
+  });
 }
 
+function reverseGeocoding(featureCollection, index, callback){
+  if(index < featureCollection.features.length){
+    var url = "http://www.mapquestapi.com/geocoding/v1/reverse?key=ZJanp0xBDpPMOLftzxBrG1tzmNtsjPWF&location=" + featureCollection.features[index].geometry.coordinates[1] + "," + featureCollection.features[index].geometry.coordinates[0];
+    var options = "&includeRoadMetadata=true&includeNearestIntersection=true";
+
+    $.getJSON(url + options, function(data){
+      var feature = featureCollection.features[index];
+      var info = data.results[0].locations[0];
+
+      if(info.nearestIntersection){
+        // feature.geometry.coordinates[0] = info.nearestIntersection.latLng.longitude;
+        // feature.geometry.coordinates[1] = info.nearestIntersection.latLng.latitude;
+        feature.properties.calle_2 = info.nearestIntersection.streetDisplayName;
+      }
+
+      if(info.roadMetadata){
+        feature.properties.limite_velocidad = info.roadMetadata.speedLimit;
+        feature.properties.limite_velocidad_unidades = info.roadMetadata.speedLimitUnits;
+      }
+
+      feature.properties.calle_1 = info.street;
+      feature.properties.codigo_postal = info.postalCode;
+    }).always(function() {
+      reverseGeocoding(featureCollection, ++index, callback);
+    });
+  }else{
+    callback(featureCollection);
+  }
+}
+
+function downloadTopDangerousResults(){
+  downloadCsv("cruces_top_incidentes_repubikla", $("#resultTopDangerous").html());
+}
 
 function clearIndicators(){
   indicatorsLayer.clearLayers();
   $("#resultTopDangerous").html("");
 
   $("#cleanFilters").trigger("click");
+  $("#downloadTopDangerous").addClass("hidden");
 }
--- client/resources/js/socket.js
+++ client/resources/js/socket.js
@@ -2,81 +2,58 @@
 var socket = io();
 
 socket.on("getPoints", function (featureCollection) {
-    window.paintPoints(featureCollection);
+  window.paintPoints(featureCollection);
 });
 
 socket.on("getRoutes", function (featureCollection) {
-    window.paintRoutes(featureCollection);
+  window.paintRoutes(featureCollection);
 });
 
 socket.on("getZones", function (featureCollection) {
-    window.paintZones(featureCollection);
+  window.paintZones(featureCollection);
 });
 
 window.insertPoint = function(geojson){
-    socket.emit("insertPoint", geojson, (geojsonResponse)=>{
-        window.insertedPoint(geojsonResponse);
-    });
+  socket.emit("insertPoint", geojson, (geojsonResponse)=>{
+    window.insertedPoint(geojsonResponse);
+  });
 };
 
 window.insertRoute = function(geojson){
-    socket.emit("insertRoute", geojson, (geojsonResponse)=>{
-        window.insertedRoute(geojsonResponse);
-    });
+  socket.emit("insertRoute", geojson, (geojsonResponse)=>{
+    window.insertedRoute(geojsonResponse);
+  });
 };
 
 window.insertZone = function(geojson){
-    socket.emit("insertZone", geojson, (geojsonResponse)=>{
-        window.insertedZone(geojsonResponse);
-    });
+  socket.emit("insertZone", geojson, (geojsonResponse)=>{
+    window.insertedZone(geojsonResponse);
+  });
 };
 
 window.downloadData = function(buttonID, dataType){
-    var type = "shp";
+  var type = "shp";
+  
+  $("#" + buttonID).button("loading");
 
-    $("#" + buttonID).button("loading");
+  var filename = "repubikla-" + dataType + ".json";
+  var eventName = buttonID;
 
-    var filename = "repubikla-" + dataType + ".json";
-    var eventName = buttonID;
-
-    socket.emit(eventName, type, (geojson)=>{
-        sendFile(geojson, filename);
-        $("#" + buttonID).button("reset");
-    });
-
-    //shpwrite.zip(geojson);
-    /*
-
-    var url = "https://ogre.adc4gis.com/convertJson";
-    var key = "json";
-    var data = JSON.stringify(geojson);
-
-    var form = $('<form></form>').attr('action', url).attr('method', 'post');
-
-    // Add the one key/value
-    form.append($("<input></input>").attr('type', 'hidden').attr('name', key).attr('value', data));
-    form.append($("<input></input>").attr('type', 'hidden').attr('name', "outputName").attr('value', "repubikla-rutas.zip"));
-
-    //send request
-    form.appendTo('body').submit().remove();
-    */
-
-    /*
-
-    $.post("https://ogre.adc4gis.com/convertJson", {json: JSON.stringify(geojson), outputName:"repubikla-rutas"}, function(result){
-
-        console.log(result);
-
-
-    });
-    */
+  socket.emit(eventName, type, (geojson)=>{
+    sendFile(geojson, filename);
+    $("#" + buttonID).button("reset");
+  });
 }
 
 function sendFile(geojson, filename){
-    var text = JSON.stringify(geojson);
+  var text = JSON.stringify(geojson);
+  var file = new File([text], filename, { type: "application/json;charset=utf-8" });
+  saveAs(file);
+}
 
-    var file = new File([text], filename, { type: "application/json;charset=utf-8" });
-    saveAs(file);
+function downloadCsv(filename, csvContent){
+  var blob = new Blob([csvContent], {type: "text/plain;charset=utf-8"});
+  saveAs(blob, filename + ".csv");
 }
 
 /*global $ moment L saveAs File*/
